f139ace04a8571049425eada2d182fe02d068976
viveking <vivekk252@gmail.com>
2016-11-30 11:09:00 +0530

    commit on Github

--- app1.js
+++ app1.js
@@ -10,18 +10,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/a',function(req,res) {
     res.send("Hello Sunil!!!");
+    
+    //localhost:3000/a;
+    
 });
 
 app.get('/a/b', function(req,res) {
     var name=req.query.name;
     var email=req.query.email;
     res.send("Hello "+name+" your email id is "+email);
+    
+    //localhost:3000/a?name=Sunil&email=sunilmondal4you@gmail.com;
 });
 
 app.get('/a/b/c',function(req,res) {
     var user={"name":req.query.name,
             "email":req.query.email};
     res.send(user);
+
+    //localhost:3000/a?name=Sunil&email=sunilmondal4you@gmail.com;
+    //answer is in the json format    {"name":"Sunil","email":"jnbgvu"} ;
 });
 
 app.listen(3000,function () {
